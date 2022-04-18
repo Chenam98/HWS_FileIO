a37d879fe8feadea8a6fa178cd92dac347945242
chena <chena@CHEN>
2022-04-18 13:19:03 +0300

    Add project files.

---
+++ HWS_FileIO/Car.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HWS_FileIO
+{
+    public class Car
+    {
+
+        //Fields & Properties
+
+        private int codan;
+        protected int NumberOfSeats;
+        public string Model { get; set; }
+        public string Brand { get; set; }
+        public string Color { get; set; }
+        public int Year { get; set; }
+
+        //Constructors
+
+        public Car()
+        {
+
+        }
+
+        public Car(string filename)
+        {
+            
+        }
+
+        public Car(int numberOfSeats, string model, string brand, string color, int year)
+        {
+            NumberOfSeats = numberOfSeats;
+            Model = model;
+            Brand = brand;
+            Color = color;
+            Year = year;
+        }
+
+        //Methods
+
+        public int GetCodan()
+        {
+            return codan;
+        }
+
+        public int GetNumberOfSeats()
+        {
+            return NumberOfSeats;
+        }
+
+        protected void ChangeNumberOfSeats(int numberOfSeats)
+        {
+            this.NumberOfSeats = numberOfSeats;
+        }
+
+        public static void SerializeACar(string fileName, Car car)
+        {
+            using (Stream serializestream = new FileStream(fileName, FileMode.Append))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(Car));
+                xml.Serialize(serializestream, car);
+            }
+        }
+        public static void SerializeACarArray(string fileName, Car[] cars)
+        {
+            using (Stream serializestream = new FileStream(fileName, FileMode.Append))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(Car));
+                xml.Serialize(serializestream, cars);
+            }   
+        }
+
+        public static Car DeserializeACar(string fileName)
+        {
+            using (Stream stream = new FileStream(fileName, FileMode.Open))
+            {
+                XmlSerializer serializer = new XmlSerializer(typeof(Car));  
+                Car dseriCar = (Car)serializer.Deserialize(stream);
+                return dseriCar;
+            }
+        }
+
+        public static Car[] DeserializeACarArray(string fileName)
+        {
+            using (Stream stream = new FileStream(fileName, FileMode.Open))
+            {
+                XmlSerializer serializerArray = new XmlSerializer(typeof(Car[]));
+                Car[] dseriCar = (Car[])serializerArray.Deserialize(stream);
+                return dseriCar;
+            }
+        }
+
+        public bool CarCompare(string fileName)
+        {
+            using (Stream stream = new FileStream(fileName, FileMode.Open))
+            {
+                XmlSerializer compserializer = new XmlSerializer(typeof(Car));
+                Car Carcompxml = (Car)compserializer.Deserialize(stream);
+            }
+                return true;
+        }
+
+        //Tostring()
+        public override string ToString()
+        {
+            return JsonSerializer.Serialize(this);
+        }
+    }
+}
---
+++ HWS_FileIO/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace HWS_FileIO
+{
+
+    public class Program
+    {
+        //Q17
+
+        //public void PrintThreeBiggestFileName(string Filename)
+        //{
+        //    DirectoryInfo directoryInfo = new DirectoryInfo(Filename);
+        //    List<FileInfo> files = directoryInfo.GetFiles().ToList();
+
+        //}
+        static void Main(string[] args)
+        {
+
+            //----------------------------------------------------------------------------------------------------
+
+            //----------------------------------------------------------------------------------------------------
+
+            Car myCar = new Car();
+            //Serialize the file
+            XmlSerializer serializer = new XmlSerializer(typeof(Car));
+            using (Stream myFile = new FileStream(@"E:\StreamEXR\Car.txt", FileMode.Create))
+            {
+                Car c1 = new Car { Model = "Christ", Brand = "Toyota", Color = "Matt Black", Year = 2019 };
+
+                serializer.Serialize(myFile, c1);
+            }
+            //Deserialize the File
+            using (Stream myFile = new FileStream(@"E:\StreamEXR\Car.txt", FileMode.Open))
+            {
+                Car ReadCarFromXml = (Car)serializer.Deserialize(myFile);
+            }
+            //Changing the xml content
+            using (Stream myFile = new FileStream(@"E:\StreamEXR\Car.txt", FileMode.Append))
+            {
+                Car c2 = new Car { Model = "Shizuru", Brand = "Hundai", Color = "White", Year = 2017 };
+
+                serializer.Serialize(myFile, c2);
+            }
+            //Deserialize the changing file
+            using (Stream myFile = new FileStream(@"E:\StreamEXR\Car.txt", FileMode.Open))
+            {
+                Car ReadCarFromXml = (Car)serializer.Deserialize(myFile);
+            }
+
+            //----------------------------------------------------------------------------------------------------
+
+            //----------------------------------------------------------------------------------------------------
+
+            XmlSerializer arraySerializer = new XmlSerializer(typeof(Car[]));
+            Car[] cars = new Car[3];
+            //Serialize the file
+            using (Stream carArray = new FileStream(@"E:\StreamEXR\carArray.txt", FileMode.Create))
+            {
+                cars[0] = new Car { Model = "A", Brand = "Lambo", Color = "green", Year = 1999 };
+                cars[1] = new Car { Model = "B", Brand = "Ford", Color = "Blue", Year = 2022 };
+                cars[2] = new Car { Model = "C", Brand = "Shevrolet", Color = "Light Blue", Year = 2006 };
+
+                arraySerializer.Serialize(carArray, cars);
+            }
+
+            //Reading the File
+            using (Stream carArray = new FileStream(@"E:\StreamEXR\carArray.txt", FileMode.Open))
+            {
+                Car ReadCarFromXml = (Car)arraySerializer.Deserialize(carArray);
+            }
+
+        }
+    }
+}
